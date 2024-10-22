35bf63c31a784e6512d8f3269843a787ee2c9403
Amulya AT <Amulya.a.t007@gmail.com>
2024-10-22 23:57:13 +0530

    created main project component

--- devfolio/src/App.js
+++ devfolio/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Headercomponent/Header';
 import Main from './Maincomponent/Main';
+import Projectone from './Projectcomponent/Projectone';
 
 function App() {
   return (
     <>
     <Header/>
     <Main/>
+    <Projectone/>
     </>
   );
 }
