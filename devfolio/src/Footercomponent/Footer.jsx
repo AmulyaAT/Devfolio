f04ad906de4058a067b0c2d5588edcf9adb4c7f0
Amulya AT <Amulya.a.t007@gmail.com>
2024-10-29 17:09:06 +0530

    added icons in footer and header components

--- devfolio/src/Footercomponent/Footer.jsx
+++ devfolio/src/Footercomponent/Footer.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./_Footer.scss"
-import "./_Mediafooter.scss"
+import "./_Mediafooter.scss";
+import email  from "./Icons/email.svg";
+import linkedin from "./Icons/linkedin.svg"
+import instagram from "./Icons/instagram.svg"
 const Footer=()=>{
     return(
         <div className="footer-container">
@@ -11,15 +14,15 @@ const Footer=()=>{
             </a>
            </div>
            <div className="icons">
-            <a href="">
-                icons
-            </a>
-            <a href="">
-                icons
-            </a>
-            <a href="">
-                icons
-            </a>
+           <a href="mailto:amulya.a.t007@gmail.com" target="_blank" rel="noopener noreferrer">
+            <img src={email} alt="Email" />
+          </a>
+          <a href="https://www.linkedin.com/in/amulya-a-t-5b522a222?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAADfrINkBbt02YZuQVfqMPWv9BVaqPks5cOw&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_people%3BQBgSfjvBS1GlyAZ2CwhZoA%3D%3D" target="_blank" rel="noopener noreferrer">
+            <img src={linkedin} alt= "Linked in"/>
+          </a>
+          <a href="https://www.instagram.com/amulya_at/profilecard/?igsh=YnZxeTUxdm9iaGky" target="_blank" rel="noopener noreferrer">
+            <img src={instagram} alt="Instagram"/>
+          </a>
            </div>
            <div className="mail">
            <a href="mailto:amulya.a.t007@gmail.com">amulya.a.t007@gmail.com</a>
--- devfolio/src/Headercomponent/Header.jsx
+++ devfolio/src/Headercomponent/Header.jsx
@@ -1,28 +1,36 @@
 import React from "react";
-import './_Header.scss'
+import "./_Header.scss";
 import "./_Mediaheader.scss";
-const Header=()=>{
-    return (
-        <div>
-        <header className="header-container">
-            <div className="logo">
-            <a href="" >Logo</a>
-            </div>
-            <div className="header">
-            <a href="">Design</a>
-            <a href="">Art</a>
-            <a href="">About</a>
-            <a href="">Contact</a>
-            </div>
-            <div className="icons">
-                <a href="">i</a>
-                <a href="">i</a>
-                <a href="">i</a>
-            </div>
-            
-        </header>
-        <hr style={{ border: "0.001rem solid #e03400"}}/>
+import email  from "./Icon/email.svg";
+import linkedin from "./Icon/linkedin.svg"
+import instagram from "./Icon/instagram.svg"
+const Header = () => {
+  return (
+    <div>
+      <header className="header-container">
+        <div className="logo">
+          <a href="">Logo</a>
         </div>
-    )
-}
-export default Header
+        <div className="header">
+          <a href="">Skills</a>
+          <a href="">Projects</a>
+          <a href="">Certificates</a>
+          <a href="">Contact</a>
+        </div>
+        <div className="icons">
+          <a href="mailto:amulya.a.t007@gmail.com" target="_blank" rel="noopener noreferrer">
+            <img src={email} alt="Email" />
+          </a>
+          <a href="https://www.linkedin.com/in/amulya-a-t-5b522a222?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAADfrINkBbt02YZuQVfqMPWv9BVaqPks5cOw&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_people%3BQBgSfjvBS1GlyAZ2CwhZoA%3D%3D" target="_blank" rel="noopener noreferrer">
+            <img src={linkedin} alt= "Linked in"/>
+          </a>
+          <a href="https://www.instagram.com/amulya_at/profilecard/?igsh=YnZxeTUxdm9iaGky" target="_blank" rel="noopener noreferrer">
+            <img src={instagram} alt="Instagram"/>
+          </a>
+        </div>
+      </header>
+      <hr style={{ border: "0.001rem solid #e03400" }} />
+    </div>
+  );
+};
+export default Header;
