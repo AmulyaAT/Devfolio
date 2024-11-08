2322bb4e851bed0c6af76feabe7dfe52a7363955
Amulya AT <Amulya.a.t007@gmail.com>
2024-11-08 16:11:22 +0530

    added certificated in certificate component

--- devfolio/src/Certificationscomponent/Certificate.jsx
+++ devfolio/src/Certificationscomponent/Certificate.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import "./_Certificate.scss";
 import "./_Mediacertificate.scss";
 
-export const Certificate = () => {
+import C from "./Certificates/C.svg";
+import Datastructure from "./Certificates/Datastructure.svg";
+import Digitalarts from "./Certificates/Digitalarts.svg";
+import Genesis from "./Certificates/Genesis.svg";
+import Internshala from "./Certificates/Internshala.svg";
+import Oasis from "./Certificates/Oasis.svg";
+import Paypal from "./Certificates/Paypal.svg";
+import Udyamsheel from "./Certificates/udyamsheel.svg";
 
+export const Certificate = () => {
   const handleNext = () => {
     const items = document.querySelectorAll(".item");
     if (items.length > 0) {
@@ -22,59 +30,37 @@ export const Certificate = () => {
     <div>
       <h2 className="mainheading">Certifications</h2>
       <div className="body">
-      <div className="box">
-        <div className="item">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoGeXSppe4nT8j48e1-6n2JwLBecraRn0TkQ&s"
-            alt="Carousel item"
-          />
+        <div className="box">
+          <div className="item">
+            <img src={C} alt="Certification C" />
+          </div>
+          <div className="item">
+            <img src={Datastructure} alt="Data Structure Certification" />
+          </div>
+          <div className="item">
+            <img src={Digitalarts} alt="Digital Arts Certification" />
+          </div>
+          <div className="item">
+            <img src={Genesis} alt="Genesis Certification" />
+          </div>
+          <div className="item">
+            <img src={Internshala} alt="Internshala Certification" />
+          </div>
+          <div className="item">
+            <img src={Oasis} alt="Oasis Certification" />
+          </div>
+          <div className="item">
+            <img src={Paypal} alt="Paypal Certification" />
+          </div>
+          <div className="item">
+            <img src={Udyamsheel} alt="Udyamsheel Certification" />
+          </div>
         </div>
-        <div className="item">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoGeXSppe4nT8j48e1-6n2JwLBecraRn0TkQ&s"
-            alt="Carousel item"
-          />
-        </div>
-        <div className="item">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoGeXSppe4nT8j48e1-6n2JwLBecraRn0TkQ&s"
-            alt="Carousel item"
-          />
-        </div>
-        <div className="item">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoGeXSppe4nT8j48e1-6n2JwLBecraRn0TkQ&s"
-            alt="Carousel item"
-          />
-        </div>
-        <div className="item">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoGeXSppe4nT8j48e1-6n2JwLBecraRn0TkQ&s"
-            alt="Carousel item"
-          />
-        </div>
-        <div className="item">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoGeXSppe4nT8j48e1-6n2JwLBecraRn0TkQ&s"
-            alt="Carousel item"
-          />
-        </div>
-        <div className="item">
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoGeXSppe4nT8j48e1-6n2JwLBecraRn0TkQ&s"
-            alt="Carousel item"
-          />
+        <div className="buttons">
+          <span className="prev" onClick={handlePrev}></span>
+          <span className="next" onClick={handleNext}></span>
         </div>
       </div>
-      <div className="buttons">
-        <span className="prev" onClick={handlePrev}>
-          
-        </span>
-        <span className="next" onClick={handleNext}>
-          
-        </span>
-      </div>
-    </div>
     </div>
   );
 };
