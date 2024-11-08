52cb876f1eda853cbc561c3c691b6f018715efa3
Amulya AT <Amulya.a.t007@gmail.com>
2024-11-08 08:36:33 +0530

    added routing to the header component

--- devfolio/src/App.js
+++ devfolio/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Footer from './Footercomponent/Footer';
 import Header from './Headercomponent/Header';
 import Main from './Maincomponent/Main';
-import {Project} from "./Projectcomponent/Project"
+import {Project} from './Projectcomponent/Project';
 import Skills from './Skillscomponent/Skills';
 import {Certificate} from './Certificationscomponent/Certificate';
 import Contact from './Contactformcomponent/Contactme';
@@ -11,14 +11,28 @@ import Time from './Timelinecomponent/Timeline';
 function App() {
   return (
     <>
-    <Header/>
-    <Main/>
-    <Skills/>
-    <Time/>
-    <Project/>
-    <Certificate/>
-    <Contact/>
-    <Footer/>
+      <Header />
+      <main>
+        <div id="main">
+          <Main />
+        </div>
+        <div id="skills">
+          <Skills />
+        </div>
+        <div id="Journey">
+          <Time />
+        </div>
+        <div id="project">
+          <Project />
+        </div>
+        <div id="certificate">
+          <Certificate />
+        </div>
+        <div id="contactme">
+          <Contact />
+        </div>
+      </main>
+      <Footer />
     </>
   );
 }
--- devfolio/src/Headercomponent/Header.jsx
+++ devfolio/src/Headercomponent/Header.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 import "./_Header.scss";
 import "./_Mediaheader.scss";
-import email  from "./Icon/email.svg";
-import linkedin from "./Icon/linkedin.svg"
-import instagram from "./Icon/instagram.svg"
+import email from "./Icon/email.svg";
+import linkedin from "./Icon/linkedin.svg";
+import instagram from "./Icon/instagram.svg";
+
 const Header = () => {
   return (
     <div>
       <header className="header-container">
         <div className="logo">
-          <a href="">Logo</a>
+          <a href="#main">Logo</a>
         </div>
         <div className="header">
-          <a href="">Skills</a>
-          <a href="">Projects</a>
-          <a href="">Certificates</a>
-          <a href="">Contact</a>
+          <a href="#skills">Skills</a>
+          <a href="#Journey">Journey</a>
+          <a href="#project">Projects</a>
+          <a href="#certificate">Certificates</a>
+          <a href="#contactme">Contact</a>
         </div>
         <div className="icons">
           <a href="mailto:amulya.a.t007@gmail.com" target="_blank" rel="noopener noreferrer">
             <img src={email} alt="Email" />
           </a>
           <a href="https://www.linkedin.com/in/amulya-a-t-5b522a222?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAADfrINkBbt02YZuQVfqMPWv9BVaqPks5cOw&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_people%3BQBgSfjvBS1GlyAZ2CwhZoA%3D%3D" target="_blank" rel="noopener noreferrer">
-            <img src={linkedin} alt= "Linked in"/>
+            <img src={linkedin} alt="LinkedIn" />
           </a>
           <a href="https://www.instagram.com/amulya_at/profilecard/?igsh=YnZxeTUxdm9iaGky" target="_blank" rel="noopener noreferrer">
-            <img src={instagram} alt="Instagram"/>
+            <img src={instagram} alt="Instagram" />
           </a>
         </div>
       </header>
@@ -33,4 +35,5 @@ const Header = () => {
     </div>
   );
 };
+
 export default Header;
